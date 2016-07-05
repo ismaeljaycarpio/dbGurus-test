7bdef4b590c17835e5d0091514e5bd6a393eaea0
x0223402 - new laptop <ismaeljaycarpio1992@gmail.com>
2016-07-05 09:11:06 +0800

    optimized helper class

--- DBGurus-Issue-Log/Default.aspx.cs
+++ DBGurus-Issue-Log/Default.aspx.cs
@@ -11,7 +11,10 @@ namespace DBGurus_Issue_Log
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if(!Page.IsPostBack)
+            {
+                Helpers.CreateDB();
+            }
         }
     }
 }
--- DBGurus-Issue-Log/Helpers.cs
+++ DBGurus-Issue-Log/Helpers.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Text;
 
 namespace DBGurus_Issue_Log
 {
@@ -24,6 +25,45 @@ namespace DBGurus_Issue_Log
             sb.Append("$('#" + modalId + "').modal('hide');");
             sb.Append(@"</script>");
             ScriptManager.RegisterClientScriptBlock(control, pageObject.GetType(), Guid.NewGuid().ToString(), sb.ToString(), false);
+        }
+
+        /// <summary>
+        /// Show Bootstrap Modal Dialog
+        /// </summary>
+        /// <param name="control">input 'this' to refer to the Control</param>
+        /// <param name="pageObj">input 'this' to refer to the Page Object</param>
+        /// <param name="modalId">Id of the div</param>
+        /// <param name="isOpen">Value whether to show/hide the modal dialog</param>
+        public static void Modal(Control control, Page pageObj, string modalId, bool isOpen)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(@"<script type='text/javascript'>");
+
+            if(isOpen)
+            {
+                sb.Append("$('#" + modalId + "').modal('show');");
+            }
+            else
+            {
+                sb.Append("$('#" + modalId + "').modal('hide');");
+            }
+            sb.Append(@"</script>");
+            ScriptManager.RegisterClientScriptBlock(control, 
+                pageObj.GetType(), 
+                Guid.NewGuid().ToString(), 
+                sb.ToString(), 
+                false);
+        }
+
+        public static void CreateDB()
+        {
+            using(var db = new IssueDataContext(@"Data Source=.\sqlexpress;Initial Catalog=dbIssue.mdf;Integrated Security=True"))
+            {
+                if(!db.DatabaseExists())
+                {
+                    db.CreateDatabase();
+                } 
+            }
         }
     }
 }
